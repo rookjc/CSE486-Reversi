533c021f36624980fcb2347e0e4bcec7782509d1
rookjc <rookjc@miamioh.edu>
2017-10-15 15:36:02 -0400

    Added Node subclass

--- Reversi_Project/src/edu/miami/cse/reversi/AI_Rook_Ritchie.java
+++ Reversi_Project/src/edu/miami/cse/reversi/AI_Rook_Ritchie.java
@@ -1,5 +1,8 @@
 package edu.miami.cse.reversi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AI_Rook_Ritchie implements Strategy {
 
 	@Override
@@ -8,4 +11,21 @@ public class AI_Rook_Ritchie implements Strategy {
 		return null;
 	}
 
+	private static class Node {
+		private Node parent;
+		private int alpha;
+		private int beta;
+		private int value; // minimax value
+		private Board board;
+		private List<Node> children;
+		
+		public Node(Node parent, int alpha, int beta, Board board) {
+			this.parent = parent;
+			this.alpha = alpha;
+			this.beta = beta;
+			this.board = board;
+			this.value = 0; // to be generated later
+			this.children = new ArrayList<>();
+		}
+	}
 }
