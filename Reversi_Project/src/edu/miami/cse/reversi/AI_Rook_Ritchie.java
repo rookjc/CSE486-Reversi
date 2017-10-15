711728682d1e3ed6a9744a235fb894b3ac233283
rookjc <rookjc@miamioh.edu>
2017-10-15 14:25:24 -0400

    Added our strategy class

---
+++ Reversi_Project/src/edu/miami/cse/reversi/AI_Rook_Ritchie.java
@@ -0,0 +1,11 @@
+package edu.miami.cse.reversi;
+
+public class AI_Rook_Ritchie implements Strategy {
+
+	@Override
+	public Square chooseSquare(Board board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
